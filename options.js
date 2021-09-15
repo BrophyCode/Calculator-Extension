26d30c97af9ef608fb4cc33de84eac9889193db0
Carson Lauer <clauer23@brophybroncos.org>
2021-09-15 09:16:48 -0700

    Added support for multiple lines drawn on the same graph.

--- options.js
+++ options.js
@@ -2,6 +2,8 @@
 minDefault = document.getElementById("minBound");
 maxDefault = document.getElementById("maxBound");
 stepDefault = document.getElementById("step");
+primaryColor = document.getElementById("primaryColor");
+secondColor = document.getElementById("secondColor");
 
 // Saves the settings to chrome storage when the submit button is pressed
 submitSettings = document.getElementById("submit").addEventListener("click", saveSettings);
@@ -11,18 +13,38 @@ function saveSettings(){
     chrome.storage.sync.set({
         "minDefault": minDefault.value, 
         "maxDefault": maxDefault.value,
-        "stepDefault": stepDefault.value}, function(){
-            console.log("Saved minDefault: " + minDefault.value);
-            console.log("Saved maxDefault: " + maxDefault.value);
-            console.log("Saved stepDefault: " + stepDefault.value);
+        "stepDefault": stepDefault.value,
+        "primaryColor": primaryColor.value,
+        "secondColor": secondColor.value}, ()=> {
             submitResults.textContent = "Successfully Saved";
     });
 }
 
 //Sets the default values for the elements
-chrome.storage.sync.get(["minDefault", "maxDefault", "stepDefault"], result => {
+async function getLocalStorageValue(){
+    return new Promise((resolve, reject) => {
+        try {
+            chrome.storage.sync.get(
+                ["minDefault",
+                "maxDefault",
+                "stepDefault",
+                "primaryColor",
+                "secondColor"],
+                 result => {resolve(result);
+            })
+        }
+        catch (ex) {
+            reject(ex);
+        }
+    });
+}
+const syncResult = getLocalStorageValue();
+
+syncResult.then(result => {
     minDefault.value = result.minDefault;
     maxDefault.value = result.maxDefault;
     stepDefault.value = result.stepDefault;
+    primaryColor.value = result.primaryColor;
+    secondColor.value = result.secondColor
 });
 
--- popup.js
+++ popup.js
@@ -10,22 +10,22 @@ class Domain {
 
         let minInput = document.createElement("input");
         minInput.setAttribute("type", "number");
-        minInput.addEventListener("input", this.updateBounds.bind(this));
+        minInput.addEventListener("input", this.update.bind(this));
         minInput.value = this.min;
 
         let maxInput = document.createElement("input");
         maxInput.setAttribute("type", "number");
-        maxInput.addEventListener("input", this.updateBounds.bind(this));
+        maxInput.addEventListener("input", this.update.bind(this));
         maxInput.value = this.max;
 
         let stepInput = document.createElement("input");
         stepInput.setAttribute("type", "number");
         stepInput.setAttribute("step", 0.01);
         stepInput.setAttribute("min", 0);
-        stepInput.addEventListener("input", this.updateBounds.bind(this));
+        stepInput.addEventListener("input", this.update.bind(this));
         stepInput.value = this.step;
 
-        divEl.append(this.variable + ": ", minInput, " < " + this.variable + " < ", maxInput, document.createElement('br'), "Step: ", stepInput);
+        divEl.append(this.variable + ": ", minInput, " < " + this.variable + " < ", maxInput, "Step: ", stepInput);
 
         minInput.setAttribute("id", "minInput");
         maxInput.setAttribute("id", "maxInput");
@@ -40,38 +40,88 @@ class Domain {
         }
         return range;
     }
-    updateBounds(event){
+    update(event){
         this.min = parseInt(document.getElementById("minInput").value);
         this.max = parseInt(document.getElementById("maxInput").value);
         this.step = parseFloat(document.getElementById("stepInput").value);
+        
+        for(let i=0; i<data.length; i++){
+            data[i]['x'] = this.range;
+            yranges[i].update();
+            data[i]['y'] = yranges[i].range;
+        }
+        updateGraph();
     }
 }
+
 
 class Equation {
     constructor(variable, equation, domain){
         this.variable = variable;
         this.equation = equation;
         this.domain = domain;
+        this.id = Math.random().toString(36).substr(2,5);
     }
     render(){
         let divEl = document.createElement('p');
         
         let eqInput = document.createElement("input");
         eqInput.setAttribute("type", "text");
-        eqInput.value = equation;
+        eqInput.addEventListener("input", this.update.bind(this) );
+        eqInput.value = this.equation;
 
-        divEl.append(this.variable + " ", eqInput);
+        divEl.append(this.variable + " = ", eqInput);
+        
+        document.getElementById("graphInputs").appendChild(divEl);
+        
+        Array.from(document.getElementsByTagName("input")).map(b=>b.style.backgroundColor = secondColor);
+    
+        eqInput.setAttribute("id", this.id);
+    }
+    get range(){
+        try{
+            let node = math.parse(this.equation);
+            let expr = node.compile();
 
-
+            let range = this.domain.map(setY);
+            function setY(value, index, array){
+                return expr.evaluate({x:value, e:math.e, pi:math.pi});
+            }
+            return range;
+        } catch(ex){
+            return ex;
+        }
+        
+    }
+    update(event) {
+        this.domain = xrange.range;
+        this.equation = document.getElementById(this.id).value;
+        for(let i=0; i<data.length; i++){
+            data[i]['y'] = yranges[i].range;
+        }
+        updateGraph();
     }
 }
 
+
+class Line {
+    constructor(x, y, type){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+}
 // Waits for the stored settings to be fetched
 // DONT CHANGE THIS
 async function getLocalStorageValue(){
     return new Promise((resolve, reject) => {
         try {
-            chrome.storage.sync.get(["minDefault", "maxDefault", "stepDefault"], result => {
+            chrome.storage.sync.get(["minDefault", 
+                                    "maxDefault", 
+                                    "stepDefault", 
+                                    "primaryColor", 
+                                    "secondColor"], 
+                                    result => {
                 resolve(result);
             })
         }
@@ -87,21 +137,64 @@ const syncResult = getLocalStorageValue();
 
 let xrange;
 let yranges = [];
+let data = [];
+
+let primaryColor = "#c9f7f8";
+let secondColor = "#00FFFF";
+
+//Creates a graph with the data
+Plotly.newPlot(graph, data,
+    {margin: {
+        l: 20,
+        r: 5,
+        b: 20,
+        t: 20,
+        pad: 0},
+    },
+);
 
 syncResult.then(result => {
     //Sets the default values for the domain element
-    xrange = new Domain("X", result.minDefault, result.maxDefault, result.stepDefault);
+    xrange = new Domain("x", result.minDefault, result.maxDefault, result.stepDefault);
     xrange.render();
+
+    // Gets the default colors
+    primaryColor = result.primaryColor;
+    secondColor = result.secondColor;
+
+    //Sets the color of the body, inputs, and buttons to the default colors
+    document.body.style.backgroundColor = primaryColor;
+    Array.from(document.getElementsByTagName("button")).map(b=>b.style.backgroundColor = secondColor);
+    Array.from(document.getElementsByTagName("input")).map(b=>b.style.backgroundColor = secondColor);
 });
 
-
+//Obtains the button to add equations
 let insertEq = document.getElementById("insertEq");
+//When the insertEq button is clicked this adds a line
 insertEq.addEventListener("click", function() {
-    yranges.push(new Equation('Y', "X", xrange));
-    console.log(xrange.range);
-    console.log(yranges[0].variable);
+    //Createds an equation that has a function y=x^2 by default
+    yranges.push(new Equation('f(x)', "x^2", xrange.range));
+    //Renders the newly created equation
+    yranges[yranges.length-1].render();
+
+    //Adds a new line to the data list
+    data.push(new Line( xrange.range, yranges[yranges.length-1].range, "lines"));
+    //Update graph
+    updateGraph();
 });
 
+
+function updateGraph(){
+    Plotly.react(graph, data,
+        {margin: {
+            l: 20,
+            r: 0,
+            b: 20,
+            t: 20,
+            pad: 0},
+        }
+    );
+}
 
 /*
 //Updates the x range
@@ -151,8 +244,8 @@ function updateGraph(){
 
 
 Plotly.newPlot(graph, [ {
-    x: xrange,
-    y: yrange,
+    x: xrange.range,
+    y: yranges[0].range,
     mode: "lines" } ],
     {margin: {
         l: 20,
@@ -161,5 +254,4 @@ Plotly.newPlot(graph, [ {
         t: 20,
         pad: 0},
     },
-);
-*/
+);*/
